01f0204e5c7885f283b69d9bcf1d47e591af5120
Dhruti <dhruti.deshpande@yale.edu>
2016-11-10 20:42:16 +0000

    event details app.js

--- app.js
+++ app.js
@@ -21,7 +21,32 @@ app.get('/', indexControllers.index);
 app.get('/about', aboutControllers.about);
 app.get('/events', eventControllers.listEvents);
 // add '//:id' and route it to a controller in eventControllers
+'use strict';
+
+// Import our express and our configuration
+var express = require('express');
+var configure = require('./config.js');
+
+// Import our controllers
+var indexControllers = require('./controllers/index.js');
+var aboutControllers = require('./controllers/about.js');
+var eventControllers = require('./controllers/events.js');
+
+
+// Create our express app
+var app = express();
+
+// Configure it
+configure(app);
+
+// Add routes mapping URLs to controllers
+app.get('/', indexControllers.index);
+app.get('/about', aboutControllers.about);
+app.get('/events', eventControllers.listEvents);
+// add '//:id' and route it to a controller in eventControllers
 app.get('/events/:id([0-9]+)', eventControllers.eventDetail);
+app.post('/events/:id', eventControllers.rsvp);
+
 app.get('/events/new', eventControllers.newEvent);
 app.post('/events/new', eventControllers.saveEvent);
 app.get('/api/events', eventControllers.api);
